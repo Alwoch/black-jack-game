89dcf814cadfc04adf2145c2a3bc268a0f537795
Alwoch Sophia <salwoch@gmail.com>
2022-10-18 07:54:45 +0000

    chore(): Add Hand class

--- main.py
+++ main.py
@@ -1,26 +1,95 @@
+#The program will have a human controlled player and a program controlled dealer
 import random
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-for suit in suits:
-  for rank in ranks:
-    cards.append([suit, rank])
+class Card:
 
+  def __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
 
-def shuffle():
-  random.shuffle(cards)
+  def __str__(self):
+    return f"{self.rank['rank']} of {self.suit}"
 
 
-def deal(number):
-  cards_dealt = []
-  for x in range(number):
-    card = cards.pop()
-    cards_dealt.append(card)
-  return cards_dealt
+class Deck:
+
+  def __init__(self):
+    #inorder to be able to access a variable in other places, we have to prepend it with the self object
+    self.cards = []
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+    ranks = [
+      {
+        "rank": "A",
+        "value": 11
+      },
+      {
+        "rank": 2,
+        "value": 2
+      },
+      {
+        "rank": 3,
+        "value": 3
+      },
+      {
+        "rank": 4,
+        "value": 4
+      },
+      {
+        "rank": 5,
+        "value": 5
+      },
+      {
+        "rank": 6,
+        "value": 6
+      },
+      {
+        "rank": 7,
+        "value": 7
+      },
+      {
+        "rank": 8,
+        "value": 8
+      },
+      {
+        "rank": 9,
+        "value": 9
+      },
+      {
+        "rank": 10,
+        "value": 10
+      },
+      {
+        "rank": "J",
+        "value": 10
+      },
+      {
+        "rank": "Q",
+        "value": 10
+      },
+      {
+        "rank": "K",
+        "value": 10
+      },
+    ]
+
+    for suit in suits:
+      for rank in ranks:
+        self.cards.append(Card(suit,rank))
+
+  def shuffle(self):
+    if len(self.cards) > 1:
+      random.shuffle(self.cards)
+
+  def deal(self, number):
+    cards_dealt = []
+    for x in range(number):
+      if len(self.cards) > 0:
+        card = self.cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
 
 
-shuffle()
-cards_dealt = deal(2)
-print(cards_dealt)
+class Hand:
+  def __init__(self):
+    self.hand=[]
